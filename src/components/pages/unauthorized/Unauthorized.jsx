f2eccaab7994910cc37a0417fc93dfba2d49b106
Informatica <informatica@alumno.local>
2025-06-03 16:45:19 -0600

    Closes #1: Configuracion inicial de Frontend

--- src/components/pages/unauthorized/Unauthorized.jsx
+++ src/components/pages/unauthorized/Unauthorized.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import "./Unauthorized.css"; // Asegúrate de tener un archivo CSS para estilos personalizados
 
 export const Unauthorized = () => (
-  <div className="container text-center mt-5">
+  <div className="unauthorized-wrapper">
+    <div className="unauthorized-content">
         <h1 className="text-danger">ACCESO DENEGADO🚫</h1>
         <p>¡¡¡Esta sección es solo para usuarios autorizados 🔒!!!</p>
         <Link to="/dashboard" className="btn btn-secondary">Volver al inicio 🏠</Link>
         <Link to="/" className="btn btn-secondary" style={{marginLeft: "10px"}}>Iniciar Sesion 🪪</Link>
-    </div>
+        </div>
+  </div>
 );
